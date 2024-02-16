1f76d4ca1091af779b90bfcd3581c1e7ebd937e3
Aurora Scalici <aurorascalici1899@icloud.com>
2024-02-16 20:22:29 +0100

    create multiplication operation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(multiplication(0.5f, 35f));
     }
 
+    public static float multiplication(float n1, float n2) {
+        return n1 * n2;
+    }
 }
