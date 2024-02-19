0b8264b2f8ca233921ef7b94661c51f66248296e
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-02-19 18:31:10 +0100

    Create method checkOperation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(checkOperation('+'));
+        System.out.println(checkOperation('-'));
+        System.out.println(checkOperation('/'));
+        System.out.println(checkOperation('*'));
+        System.out.println(checkOperation('^'));
+        System.out.println(checkOperation('&'));
+    }
 
+    public static String checkOperation (char operator) {
+        String operationName = "";
+        switch (operator) {
+            case '+': 
+                operationName = "Addition";
+                break;
+            case '-':
+                operationName = "Subtraction";
+                break;
+            case '*':
+                operationName = "Multiplication";
+                break;
+            case '/':
+                operationName = "Division";
+                break;
+            case '^':
+                operationName = "Exponents";
+                break;
+            default:
+                operationName = "Not an operation";
+
+        }
+        return  operationName;
     }
 
 }
