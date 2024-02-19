ff0a640168b4d95886576b19cc09174e5dc86b97
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-02-19 20:26:59 +0100

    Add checkMethod

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(checkOperation('+'));
-        System.out.println(checkOperation('-'));
-        System.out.println(checkOperation('/'));
-        System.out.println(checkOperation('*'));
-        System.out.println(checkOperation('^'));
-        System.out.println(checkOperation('&'));
+        System.out.println("Type of operation is = " + checkOperation(5, '/', 9));
+        System.out.println("Type of operation is = " + checkOperation(5, '*', 9));
+        System.out.println("Type of operation is = " + checkOperation(5, '-', 9));
+        System.out.println("Type of operation is = " + checkOperation(5, '+', 9));
+        System.out.println("Type of operation is = " + checkOperation(5, '^', 9));
+        System.out.println("Type of operation is = " + checkOperation(5, '&', 9));
+        System.out.println();
+
+        System.out.println("Type of operation is = " + checkOperator("15 + 89"));
+        System.out.println("Type of operation is = " + checkOperator("15 / 89"));
+        System.out.println("Type of operation is = " + checkOperator("15 * 89"));
+        System.out.println("Type of operation is = " + checkOperator("15 - 89"));
+        System.out.println("Type of operation is = " + checkOperator("15 ^ 89"));
+        System.out.println("Type of operation is = " + checkOperator("15 & 89"));
+
     }
 
-    public static String checkOperation (char operator) {
+    public static String checkOperation(float n1, char operator, float n2) {
         String operationName = "";
         switch (operator) {
-            case '+': 
-                operationName = "Addition";
+            case '+':
+                operationName = "Sum";
                 break;
             case '-':
                 operationName = "Subtraction";
@@ -31,7 +40,27 @@ public class Main {
                 operationName = "Not an operation";
 
         }
-        return  operationName;
+        return operationName;
+    }
+
+    public static String checkOperator(String operation) {
+        if (operation.contains("+")) {
+            return "Sum";
+        }
+        if (operation.contains("-")) {
+            return "Subtraction";
+        }
+        if (operation.contains("/")) {
+            return "Division";
+        }
+        if (operation.contains("*")) {
+            return "Multiplication";
+        }
+        if (operation.contains("^")) {
+            return "Exponents";
+        } else {
+            return "This is not an operation";
+        }
     }
 
 }
