abe40371eba75fe7a2f20cd403f3f50ce2642f52
Ttamer <mericomattia@gmail.com>
2024-02-19 19:08:50 +0100

    renamed sumFunction in getSum, added more case to test the method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sumFunction(8, 11));
+        System.out.println(getSum(8, 11));
+        System.out.println(getSum(-6, -4));
+        System.out.println(getSum(-12, 10));
+        System.out.println(getSum(38, 45));
+        System.out.println(getSum(0, -6));
     }
-    public static int sumFunction (int n1, int n2) {
+
+    public static int getSum(int n1, int n2) {
         return n1 + n2;
     }
 
