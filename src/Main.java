7e10148ef53ed292ea9b04d11170d1ee987d28a3
SIlvioVutea <silviovutea@gmail.com>
2024-02-20 17:52:53 +0100

    Add Subtraction method with float variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sumFunction(8, 11));
-    }
-    public static int sumFunction (int n1, int n2) {
-        return n1 + n2;
+        System.out.println(getSubtraction(5, 2));
     }
 
+    public static double getSubtraction(float n1, float n2) {
+        return n1 - n2;
+    }
 }
