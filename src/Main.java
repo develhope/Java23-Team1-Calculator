f834c75d9944adbd1b034ce553db4ac09f3f69ca
serenannalacerenza <tua@email.com>
2024-02-20 17:09:15 +0100

    aggiunto metodo pow

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.err.println("la potenza è" + pow(2, 4));
+    }
 
+    public static int pow(int base, int exponent) {
+        int result = 1;
+        for (int i = 0; i < exponent; i++) {
+            result = result * base;
+
+        }
+        return result;
     }
 
 }
