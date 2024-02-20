69489bf9b8387f11df166480ac939b73e9ee2db1
SIlvioVutea <silviovutea@gmail.com>
2024-02-20 11:04:11 +0100

    Add subtraction

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getSubtraction(5, 7));
+        System.out.println(getSubtraction(5, -798889927));
     }
 
-    public static float getSubtraction(float n1, float n2) {
+    public static double getSubtraction(double n1, double n2) {
         return n1 - n2;
     }
 }
