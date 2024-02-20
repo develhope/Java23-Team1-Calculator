586e3e11e333841d07f7bab47ada2b0cf481a5d9
serenannalacerenza <tua@email.com>
2024-02-20 20:46:55 +0100

    added function method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        int result = getPow(0, 0);
+        System.out.println("result: " + result);
     }
 
+    public static int getPow(int base, int exponent) {
+        int result = 1;
+
+        for (int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
 }
