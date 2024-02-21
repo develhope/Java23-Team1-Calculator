ad89e4c17e984981d32009642d0082f115ea8510
Aurora Scalici <aurorascalici1899@icloud.com>
2024-02-21 10:10:35 +0100

    change float - int

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getMulptiplication(0.5f, 35f));
-        System.out.println(getMulptiplication(-5, -35));
-        System.out.println(getMulptiplication(8f, -10f));
+        System.out.println(getMultiplication(-5, -35));
+        System.out.println(getMultiplication(8, -10));
     }
 
-    public static float getMulptiplication(float n1, float n2) {
+    public static int getMultiplication(int n1, int n2) {
         return n1 * n2;
     }
 }
