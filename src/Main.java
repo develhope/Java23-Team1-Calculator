179c8d4048a73f12fee90602961b5858ae18412b
Aurora Scalici <aurorascalici1899@icloud.com>
2024-02-21 10:04:46 +0100

    file ripristinato

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,9 @@ public class Main {
         System.out.println(getMulptiplication(0.5f, 35f));
         System.out.println(getMulptiplication(-5, -35));
         System.out.println(getMulptiplication(8f, -10f));
-        System.out.println(getMulptiplication(999999999f, 1f));
     }
 
     public static float getMulptiplication(float n1, float n2) {
-
-        if (n1 > 999999999f) {
-            System.out.println("Err");
-        }
-        if (n2 > 999999999f) {
-            System.out.println("Err");
-        }
         return n1 * n2;
     }
 }
